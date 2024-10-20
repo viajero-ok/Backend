7369d5640cf90c8fb81a51a5ffaf8cd77d0739cb
Mariano Luque <mn.luque.14@gmail.com>
2024-10-20 19:34:02 -0300

    pagos

--- src/modules/pagos/pagos.controller.ts
+++ src/modules/pagos/pagos.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query, Redirect } from '@nestjs/common';
 import { PagosService } from './pagos.service';
+import { Public } from 'src/common/decorators/public/public.decorator';
 
 @Controller('pagos')
 export class PagosController {
@@ -8,5 +9,22 @@ export class PagosController {
 	@Post('generar-orden')
 	async generarOrden() {
 		return this.pagosService.generarOrden();
+	}
+
+	@Public()
+	@Get('oauth')
+	async oauthCallback(@Query('code') code: string) {
+		/* return this.pagosService.oauthCallback(code); */
+		console.log(code);
+	}
+
+	@Get('solicitar-autorizacion-prestador')
+	@Redirect()
+	async solicitarAutorizacionPrestador() {
+		const appId = process.env.MERCADO_PAGO_APP_ID;
+		const redirectUri = process.env.REDIRECT_URI;
+		return {
+			url: `https://auth.mercadopago.com.ar/authorization?client_id=${appId}&response_type=code&platform_id=mp&redirect_uri=${redirectUri}`,
+		};
 	}
 }
