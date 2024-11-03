7092d3d834c2d31b7ee5f026e7684f8b7d577723
Mariano Luque <mn.luque.14@gmail.com>
2024-11-03 15:57:39 -0300

    fix mp

--- src/modules/oferta-turistica/actividades/pestaña-actividad/dto/eliminar-guia.dto.ts
+++ src/modules/oferta-turistica/actividades/pestaña-actividad/dto/eliminar-guia.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class EliminarGuiaDto {
 	@ApiProperty({
@@ -12,9 +12,9 @@ export class EliminarGuiaDto {
 
 	@ApiProperty({
 		description: 'ID de la guía',
-		example: '123e4567-e89b-12d3-a456-426614174000',
+		example: 1,
 	})
-	@IsString()
+	@IsNumber()
 	@IsNotEmpty()
-	id_guia: string;
+	id_guia: number;
 }
