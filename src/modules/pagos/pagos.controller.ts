dcca94449889bcd5fa8058612508a0084bc32ccb
Mariano Luque <mn.luque.14@gmail.com>
2024-11-05 13:18:10 -0300

    fix mp

--- src/modules/pagos/pagos.controller.ts
+++ src/modules/pagos/pagos.controller.ts
@@ -59,6 +59,9 @@ export class PagosController {
 	@ApiOperation({ summary: 'NOTIFICACIÓN DE PAGO (WEBHOOK)' })
 	@Post('notification')
 	async notification(@Req() req) {
+		console.log('NOTIFICATION');
+		console.log('REQUEST QUERY', req.query);
+		console.log('REQUEST', req);
 		return this.pagosService.notification(req);
 	}
 
