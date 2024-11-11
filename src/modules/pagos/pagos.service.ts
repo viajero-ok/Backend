a4543093aebc35ed24ffe9170255b9a98c4e9a86
Mariano Luque <mn.luque.14@gmail.com>
2024-11-10 22:13:56 -0300

    fix mp

--- src/modules/pagos/pagos.service.ts
+++ src/modules/pagos/pagos.service.ts
@@ -181,14 +181,13 @@ export class PagosService {
 		// Determinar el tipo de notificación
 		if (req.query.type === 'payment') {
 			console.log('PAYMENT NOTIFICATION');
-			console.log('REQUEST QUERY DATA', req.query.data);
 			const id_pago = req.query['data.id'];
 			console.log('ID PAGO', id_pago);
 			const client = new MercadoPagoConfig({
 				accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
 			});
 			const payment = new Payment(client);
-			const response = await payment.get({ id: req.query.id });
+			const response = await payment.get({ id: id_pago });
 			console.log('PAYMENT RESPONSE', response);
 
 			try {
