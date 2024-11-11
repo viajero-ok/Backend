cacbb4a497dfbf5820a76c9a7fb23cf2f4317028
Mariano Luque <mn.luque.14@gmail.com>
2024-11-10 21:26:49 -0300

    fix mp

--- src/modules/pagos/pagos.service.ts
+++ src/modules/pagos/pagos.service.ts
@@ -188,7 +188,7 @@ export class PagosService {
 				accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
 			});
 			const payment = new Payment(client);
-			const response = await payment.search(id_pago);
+			const response = await payment.get({ id: id_pago });
 			console.log('PAYMENT RESPONSE', response);
 		} else if (req.query.type === 'merchant_order') {
 			console.log('MERCHANT ORDER NOTIFICATION');
