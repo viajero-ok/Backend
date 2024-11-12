5740fa45635cf0f418655769d025d9424c82e0d3
Mariano Luque <mn.luque.14@gmail.com>
2024-11-12 17:28:15 -0300

    fix mp

--- src/modules/pagos/pagos-repository.service.ts
+++ src/modules/pagos/pagos-repository.service.ts
@@ -114,7 +114,7 @@ export class PagosRepositoryService {
 	async registrarPago(datos_pago: any) {
 		console.log(datos_pago);
 		const result = await this.entityManager.query(
-			'CALL SP_REGISTRAR_PAGO_RESERVA(?, ?, ?, ?, ?, ?)',
+			'CALL SP_REGISTRAR_PAGO_RESERVA(?, ?, ?, ?, ?, ?, ?, ?)',
 			[
 				datos_pago.id,
 				datos_pago.external_reference,
